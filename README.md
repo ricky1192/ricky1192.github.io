949901aba85ebe0b3031a45e7357de6273740823
Riccardo Cosmai <riccardo.cosmai@enel.com>
2023-08-23 17:09:44 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ricky1192.github.io
+This project was with built with the help of the MaterializeCSS framework
---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function ($) {
+  $(function () {
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
+    $(".dropdown-trigger").dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
